5cba6e9e2442e83b049b9e43791239a6cdbc9eb4
Alvin <alvin.webb@icloud.com>
2017-02-22 09:31:49 -0500

    2/22/2017 9:31AM

---
+++ main.php
@@ -0,0 +1,147 @@
+<div class="" id="meet-me"></div>
+<br>
+<div class="container-fluid" >
+  <section >
+    <div class="jumbotron">
+      <div class="about-me">
+        <div class="row">
+          <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+            <img src="img/me.jpg" alt="Picture of Alvin">
+          </div>
+          <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 about">
+            <h2>My Name is Avin</h2>
+            <p>
+              I have had a interest in Technology since I was 13. I would tinker with computers I would find in thrown away. <br>
+              I learned CompTia A+ when I was at <a href="#">Job Corps</a>. I was 19 I started my Tech Career fixing computers. <br>
+              I'm currently working for <a href="#">KellySerivces</a>, I have been with the company Almost 3 years working as Technical Support for Apple Products. <br>
+              I always wanted to learn how to write code, and I have been working in Technical Support for around 8 Years. I was ready for a new Challange... <br>
+              I have been learning my skill from online resourses like <a href="#">FreeCodeCamp</a>, <a href="#">Udemy</a>, <a href="#">Lynda</a>, and <a href="#">Skillcrush</a>. <br>
+              I have also been reading Jon Ducketts Books on HTML/CSS and currently Javascript/jQuery.<br>
+              As of 2/27/2017 I have Started attending Full Sail university. Degree: Bachelor of Science Web Design &amp; Development.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section id="portfolio">
+    <div class="portfolio">
+      <div class="row">
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="#" class="thumbnail">
+            <img src="http://placehold.it/500x300">
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="#" class="thumbnail">
+            <img src="http://placehold.it/500x300">
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="#" class="thumbnail">
+            <img src="http://placehold.it/500x300">
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="#" class="thumbnail">
+            <img src="http://placehold.it/500x300">
+          </a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="#" class="thumbnail">
+            <img src="http://placehold.it/500x300">
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="#" class="thumbnail">
+            <img src="http://placehold.it/500x300">
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="#" class="thumbnail">
+            <img src="http://placehold.it/500x300">
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <a href="#" class="thumbnail">
+            <img src="http://placehold.it/500x300">
+          </a>
+        </div>
+      </div>
+
+
+    </div>
+  </section>
+  <section id="contact">
+    <div class="row">
+      <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+        <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+          <div class="panel panel-default">
+            <div class="panel-heading" role="tab" id="headingOne">
+              <h4 class="panel-title">
+                <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                  Basic Package
+                </a>
+              </h4>
+            </div>
+            <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+              <div class="panel-body">
+                Basic Body
+              </div>
+            </div>
+          </div>
+          <div class="panel panel-default">
+            <div class="panel-heading" role="tab" id="headingTwo">
+              <h4 class="panel-title">
+                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                  Advance Package
+                </a>
+              </h4>
+            </div>
+            <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+              <div class="panel-body">
+                Advance Body
+              </div>
+            </div>
+          </div>
+          <div class="panel panel-default">
+            <div class="panel-heading" role="tab" id="headingThree">
+              <h4 class="panel-title">
+                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                  Premium Package
+                </a>
+              </h4>
+            </div>
+            <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+              <div class="panel-body">
+                Premium Body
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+        <h4 class="text-center">Let's Connect!</h4>
+        <div class="sent-email">
+          
+        </div>
+        <form action="" method="post">
+          First Name: <input type="text" name="first_name"><br>
+          Last Name: <input type="text" name="last_name"><br>
+          Email: <input type="text" name="email"><br>
+          Subject: <select name="form_subject">
+                      <option value="basic-package">Basic Package</option>
+                      <option value="advance-package">Advance Package</option>
+                      <option value="premium-package">Premium Package</option>
+                  </select><br>
+          Message:<br><textarea rows="5" name="message" cols="30"></textarea><br>
+          <input type="submit" name="submit" value="Submit">
+        </form>
+
+
+      </div>
+    </div>
+  </section>
+  </div>
